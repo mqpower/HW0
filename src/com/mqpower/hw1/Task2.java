25a0cf5177f2ec329cade6b8371a49edbf1d66dd
@ <mqpower1@gmail.com>
2019-10-12 18:13:40 +0300

    add Task2

--- src/com/mqpower/hw1/Task2.java
+++ src/com/mqpower/hw1/Task2.java
@@ -1,4 +1,11 @@
 package com.mqpower.hw1;
 
 public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("*     *  *  ****  *  *      *");
+        System.out.println("* * * *  *  *     *  *     * *");
+        System.out.println("*  *  *  *  ****  ****    *****");
+        System.out.println("*     *  *     *  *  *   *     *");
+        System.out.println("*     *  *  ****  *  *  *       *");
+    }
 }
